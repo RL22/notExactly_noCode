e85113fce398f1753f51eb57e65db45dcc7bf21a
Rodney L. Lewis <lewis.rodneyl@gmail.com>
2023-12-18 10:36:20 -0800

    Updated truncate.js
    + nearest %20

--- truncate.js
+++ truncate.js
@@ -3,12 +3,16 @@
 const elements = document.querySelectorAll('[truncate-text]');
 
 elements.forEach((element) => {
- // Get the character limit as an integer, defaulting to 0 if not set
- const charLimit = parseInt(element.getAttribute('truncate-text'), 10) || 0;
+  // Get the character limit as an integer, defaulting to 0 if not set
+  const charLimit = parseInt(element.getAttribute('truncate-text'), 10) || 0;
 
- // Truncate the text content of the element if it exceeds the character limit
- if (element.textContent.length > charLimit) {
-  element.textContent = element.textContent.substring(0, charLimit) + '...';
- }
+  if (element.textContent.length > charLimit) {
+    // Find the nearest space before the character limit
+    let nearestSpace = element.textContent.lastIndexOf(' ', charLimit);
+    nearestSpace = nearestSpace > 0 ? nearestSpace : charLimit;
+
+    // Truncate the text content at the nearest space
+    element.textContent = element.textContent.substring(0, nearestSpace) + '...';
+  }
 });
 </script>
